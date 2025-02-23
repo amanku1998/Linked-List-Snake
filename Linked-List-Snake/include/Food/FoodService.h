d6fdee04e84751927ef468ae8d6d05e4f673d28e
Aman Kumar <aman.kumar01747@gmail.com>
2025-02-23 17:30:20 +0530

    -Setup logic to spawning a food in a specific intervals

--- Linked-List-Snake/include/Food/FoodService.h
+++ Linked-List-Snake/include/Food/FoodService.h
@@ -8,6 +8,12 @@ namespace Food
 	enum class FoodType;
 	class FoodItem;
 
+	enum FoodSpawningStatus
+	{
+		ACTIVE,
+		IN_ACTIVE,
+	};
+
 	class FoodService
 	{
 	private:
@@ -19,10 +25,24 @@ namespace Food
 		std::default_random_engine random_engine;
 		std::random_device random_device;
 
+		const float spawn_duration = 4.f;
+		float elapsed_duration;
+
+		FoodSpawningStatus current_spawning_status;
+
 		FoodItem* createFood(sf::Vector2i position, FoodType type);
 		void spawnFood();
 
+		sf::Vector2i getValidSpawnPosition();
+		sf::Vector2i getRandomPosition();
+		FoodType getRandomFoodType();
+
+		bool isValidPosition(std::vector<sf::Vector2i> position_data, sf::Vector2i food_position);
+
 		void destroyFood();
+		void updateElapsedDuration();
+		void handleFoodSpawning();
+		void reset();
 
 	public:
 		FoodService();
@@ -33,11 +53,6 @@ namespace Food
 		void render();
 
 		void startFoodSpawning();
-		sf::Vector2i getRandomPosition();
-
-		bool isValidPosition(std::vector<sf::Vector2i> position_data, sf::Vector2i food_position);
-		sf::Vector2i getValidSpawnPosition();
-
-		FoodType getRandomFoodType();
+		void stopFoodSpawning();
 	};
 }
--- Linked-List-Snake/source/Food/FoodService.cpp
+++ Linked-List-Snake/source/Food/FoodService.cpp
@@ -24,11 +24,17 @@ namespace Food
 
 	void FoodService::initialize()
 	{
-		
+		elapsed_duration = spawn_duration;
 	}
 
 	void FoodService::update()
 	{
+		if (current_spawning_status == FoodSpawningStatus::ACTIVE)
+		{
+			updateElapsedDuration();
+			handleFoodSpawning();
+		}
+
 		if (current_food_item) current_food_item->update();
 	}
 
@@ -39,10 +45,17 @@ namespace Food
 
 	void FoodService::startFoodSpawning()
 	{
+		current_spawning_status = FoodSpawningStatus::ACTIVE;
+
 		cell_width = ServiceLocator::getInstance()->getLevelService()->getCellWidth();
 		cell_height = ServiceLocator::getInstance()->getLevelService()->getCellHeight();
+	}
 
-		spawnFood();
+	void FoodService::stopFoodSpawning()
+	{
+		current_spawning_status = FoodSpawningStatus::IN_ACTIVE;
+		destroyFood();
+		reset();
 	}
 
 	FoodItem* FoodService::createFood(sf::Vector2i position, FoodType type)
@@ -102,5 +115,25 @@ namespace Food
 	{
 		std::uniform_int_distribution<int> distribution(0, FoodItem::number_of_foods - 1);
 		return static_cast<FoodType>(distribution(random_engine));
+	}
+
+	void FoodService::reset()
+	{
+		elapsed_duration = 0.f;
+	}
+
+	void FoodService::updateElapsedDuration()
+	{
+		elapsed_duration += ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+	}
+
+	void FoodService::handleFoodSpawning()
+	{
+		if (elapsed_duration >= spawn_duration)
+		{
+			destroyFood();
+			reset();
+			spawnFood();
+		}
 	}
 }
