d870ab834cd5ed483b70b9b3f307f4be03298561
Aman Kumar <aman.kumar01747@gmail.com>
2025-02-24 19:09:32 +0530

    -Insert new node at the tail

--- Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
@@ -81,12 +81,15 @@ namespace LinkedList
 	}
 
 	void SingleLinkedList::insertNodeAtTail() {
+
+		linked_list_size++;
+
 		Node* new_node = createNode();
 		Node* cur_node = head_node;
 
 		if (cur_node == nullptr) {       // If the list is empty, set the new node as the head
 			head_node = new_node;
-			new_node->body_part.initialize(node_width, node_height, default_position, default_direction);
+			initializeNode(new_node, nullptr, Operation::TAIL);
 			return;
 		}
 
@@ -97,7 +100,7 @@ namespace LinkedList
 
 		// Attach the new node at the end
 		cur_node->next = new_node;
-		new_node->body_part.initialize(node_width, node_height, getNewNodePosition(cur_node, Operation::TAIL), cur_node->body_part.getDirection());
+		initializeNode(new_node, cur_node, Operation::TAIL);
 	}
 
 	void SingleLinkedList::updateNodeDirection(Direction direction_to_set)
