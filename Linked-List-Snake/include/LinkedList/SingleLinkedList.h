9912cf02ab9ac865a28bac2cc5e03979b3bc6274
Aman Kumar <aman.kumar01747@gmail.com>
2025-02-25 21:07:48 +0530

    -Split in half

--- Linked-List-Snake/include/LinkedList/SingleLinkedList.h
+++ Linked-List-Snake/include/LinkedList/SingleLinkedList.h
@@ -49,6 +49,8 @@ namespace LinkedList
 		void removeNodeAtMiddle();
 		void removeNodeAtTail();
 		Node* getHeadNode();
+		Node* findNodeAtIndex(int index);
+		void removeHalfNodes();
 		std::vector<sf::Vector2i> getNodesPositionList();
 		sf::Vector2i getNewNodePosition(Node* reference_node, Operation operation);
 		void initializeNode(Node* new_node, Node* reference_node, Operation operation);
--- Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
@@ -327,6 +327,43 @@ namespace LinkedList
 		cur_node->next = nullptr; //Set the new tail node's next pointer to nullptr
 	}
 
+	Node* SingleLinkedList::findNodeAtIndex(int index)
+	{
+		int current_index = 0;
+		Node* cur_node = head_node;
+		Node* prev_node = nullptr;
+
+		while (cur_node != nullptr && current_index <= index)
+		{
+			prev_node = cur_node;
+			cur_node = cur_node->next;
+			current_index++;
+		}
+
+		return prev_node;
+	}
+
+	void SingleLinkedList::removeHalfNodes()
+	{
+		if (linked_list_size <= 1) return;
+		int half_length = linked_list_size / 2;
+		int new_tail_index = half_length - 1;
+
+		Node* prev_node = findNodeAtIndex(new_tail_index);
+		Node* cur_node = prev_node->next;
+
+		while (cur_node != nullptr)
+		{
+			Node* node_to_delete = cur_node;
+			cur_node = cur_node->next;
+
+			delete (node_to_delete);
+			linked_list_size--;
+		}
+
+		prev_node->next = nullptr;
+	}
+
 	std::vector<sf::Vector2i> SingleLinkedList::getNodesPositionList()
 	{
 		std::vector<sf::Vector2i> nodes_position_list;
